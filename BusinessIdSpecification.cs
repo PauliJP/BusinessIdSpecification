3ffcd96a4e8d8cf958765eeba3e780be1d52c49e
Pauli <vidinet.net@gmail.com>
2018-01-27 18:14:18 +0200

    fixing the findings from unit tests

--- BusinessIdSpecification.cs
+++ BusinessIdSpecification.cs
@@ -32,16 +32,17 @@ namespace CGIExcercise
 		/// <returns>true if businessId is valid</returns>
 		public bool IsSatisfiedBy(string businessId)
 		{
+			_ReasonsForDissatisfaction = new List<string>();	// before each run, empty the errors list
 			bool testOk = true;
 			int[] multiplyOperands = { 7, 9, 10, 5, 8, 4, 2 };
 			
 			// static analysis
-			if (businessId.Length == 0 || businessId.Length > 9) {
+			if (businessId.Length != 9) {
 				_ReasonsForDissatisfaction.Add(SpecificationHelper.OutOfRange);
 				testOk = false;
 			}
 			for (int i = 0; i < businessId.Length; i++) {
-				if ((i != 7) && !Char.IsNumber(businessId[i]) || (i == 7) && businessId[i] == '-') {
+				if ((i != 7) && !Char.IsNumber(businessId[i]) || (i == 7) && businessId[i] != '-') {
 					_ReasonsForDissatisfaction.Add(SpecificationHelper.SyntacticallyIncorrect);
 					testOk = false;
 					break;
@@ -51,7 +52,7 @@ namespace CGIExcercise
 			// check digit analysis
 			int sumOfProducts = 0;
 			for (int i = 0; i < businessId.Length && i < multiplyOperands.Length; i++) {
-				sumOfProducts += multiplyOperands[i] * businessId[i];
+				sumOfProducts += multiplyOperands[i] * (int)Char.GetNumericValue(businessId[i]);
 			}
 			sumOfProducts = sumOfProducts % 11;
 			int checksum = 0;
@@ -66,7 +67,9 @@ namespace CGIExcercise
 			
 			// the last character in businessId must be the same as checksum
 			if (checksum != (int)Char.GetNumericValue(businessId[businessId.Length - 1])) {
-				_ReasonsForDissatisfaction.Add(SpecificationHelper.CheckDigitFailed);
+				if (!_ReasonsForDissatisfaction.Contains(SpecificationHelper.CheckDigitFailed)) {
+					_ReasonsForDissatisfaction.Add(SpecificationHelper.CheckDigitFailed);
+			    }
 				testOk = false;
 			}
 			
